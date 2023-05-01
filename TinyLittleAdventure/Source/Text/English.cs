6d7d1f958e747524eeb3b4336c3f7851412945e9
Georgios Zacharidis <43867022+zacharidis@users.noreply.github.com>
2023-05-01 20:17:53 +0300

    added Text and Language classes

---
+++ TinyLittleAdventure/Source/Text/English.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyLittleAdventure.Source.Text
+{
+    public class English : Language
+    {
+
+
+
+    }
+}
---
+++ TinyLittleAdventure/Source/Text/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyLittleAdventure.Source.Text
+{
+    public abstract class Language
+    {
+        public string Welcome { get; protected set; } = "";
+        public string ChooseYourName { get; protected set; } = "";
+
+        public string DefaultName { get; protected set; } = "";
+    }
+}
