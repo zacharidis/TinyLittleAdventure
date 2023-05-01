1df82c26cf136a6f5f9dd67a127ce89d2f2861bc
Georgios Zacharidis <zacharidis@icloud.com>
2023-05-01 19:41:11 +0300

    Add project files.

---
+++ TinyLittleAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello there , are you ready for an adventure ?");
+Console.WriteLine("Enter your name and hit enter");
+
+var playersName = Console.ReadLine();
+
+if (playersName == String.Empty)
+{
+    playersName = "No Name";
+}
+
+Console.WriteLine($"Welcome {playersName}");
+
